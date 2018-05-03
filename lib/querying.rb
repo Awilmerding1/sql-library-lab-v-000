176910f911cba4893c387b9ce820f82b05f1f3a4
Annabel Wilmerding <annabelwilmerding@gmail.com>
2018-05-03 15:17:19 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -40,7 +40,7 @@ def select_character_names_and_number_of_books_they_are_in
   "SELECT characters.name, COUNT(books.title)
   FROM characters
   INNER JOIN character_books
-  ON character_books.character_id = characters.id
+  ON characters.id = character_books.character_id
   INNER JOIN books
   ON character_books.book_id = books.id"
 end
