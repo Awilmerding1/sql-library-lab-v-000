3a4c391f92199bd6cffddf694e3a1ab035aaeefe
Annabel Wilmerding <annabelwilmerding@gmail.com>
2018-05-03 14:52:15 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -20,9 +20,9 @@ def select_value_and_count_of_most_prolific_species
 end
 
 def select_name_and_series_subgenres_of_authors
-  "SELECT authors.name, subgenres.name
-  FROM series INNER JOIN subgenres
-  ON series.author_id "
+  "SELECT authors.name
+  FROM authors INNER JOIN series
+  ON authors.id = series.author_id"
 end
 
 def select_series_title_with_most_human_characters
